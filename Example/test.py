5e6f6d06be3704613e8d13d9ff9bad1cc340380b
JX <sidewallme@gmail.com>
2016-03-14 02:32:27 -0500

    modify test file

--- Example/test.py
+++ Example/test.py
@@ -4,4 +4,5 @@ from quantreg.qr import quantreg
 
 data = pd.DataFrame(sm.datasets.engel.load_pandas().data)
 qr_model = quantreg()
-qr_model.fit_pogs(data[['foodexp', ]], data['income'], 0.3)
+fit  = qr_model.fit_pogs(data[['foodexp', ]], data['income'], 0.3)
+print(fit)
